dc9f91cd3d7ef7d0112390a709d017b058ed7698
zhangjia517@hotmail.com <zhangjia517@hotmail.com>
2016-07-18 15:58:21 +0800

    Init

---
+++ Client/Assets/Script/TempMgr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TempMgr : MonoBehaviour
+{
+    public UITexture m_Pic = null;
+
+    private string m_url = "http://www.fostudio.cn/zhangjia/app_pic/app_pic_3.jpg";
+
+    private IEnumerator Start()
+    {
+        WWW www = new WWW(m_url);
+        yield return www;
+
+        m_Pic.mainTexture = www.texture;
+        m_Pic.height = www.texture.height;
+        m_Pic.width = www.texture.width;
+    }
+
+    private void Update()
+    {
+
+    }
+}
